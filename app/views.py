18677b4e961a0250d55c5ff8519e967e4fd82f8b
Sam Kreter <samkreter@gmail.com>
2016-11-09 15:08:14 -0600

    adding frontend template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
+from flask import Flask, render_template, request, jsonify
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
