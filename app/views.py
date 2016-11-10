ac27ae1df5d85ed6f5f436871d1901c04471cdf8
Sam Kreter <samkreter@gmail.com>
2016-11-09 21:52:06 -0600

    almost all good with the funcinallity

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from app import app, models
+from app import app, models, db
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +7,18 @@ def index():
 
     posts = models.Post.query.all()
     return render_template('index.html',posts=posts)
+
+@app.route('/posts',methods=['GET'])
+def posts():
+    title = request.args.get('title')
+    body = request.args.get('body')
+    p = models.Post(body=body, title=title)
+    db.session.add(p)
+    db.session.commit()
+    return jsonify(
+        {
+            'error':1,
+            'title':title,
+            'body':body
+
+        })
